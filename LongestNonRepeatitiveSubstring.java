9a3d78cb79a7c158393bd45a4abb4f684771279e
David Kitavi <geospatialeng2017@gmail.com>
2022-12-31 14:11:53 +0300

    strings

---
+++ LongestNonRepeatitiveSubstring.java
@@ -0,0 +1,40 @@
+package strings;
+
+import java.util.HashMap;
+
+public class LongestNonRepeatitiveSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+        int max = 0, start = 0, i = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        while(i < s.length()) {
+            char temp = s.charAt(i);
+
+            // adding key to map if not present
+            if(!map.containsKey(temp)) map.put(temp, 0);
+            // checking if the first value is the duplicate value
+            else if (s.charAt(start) == temp) start++;
+            // chacking if the previous value is duplicate value
+            else if (s.charAt(i -1) == temp) {
+                if(max < map.size()) max = map.size();
+                map = new HashMap<>();
+                start = i;
+                 i--;
+            }
+            // last possible place where duplicate value can be is between start and i
+            else {
+                if(max < map.size()) max = map.size();
+                while (s.charAt(start) != temp) {
+                    map.remove(s.charAt(start));
+                    start++;
+                }
+                start++;
+            }
+
+            i++;
+        }
+        if(max < map.size()) max = map.size();
+
+        return max;
+    }
+}
